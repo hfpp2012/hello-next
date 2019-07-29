8448dbce0f905e8896e57f6827a75ed5a72eb5df
hfpp2012 <hfpp2012@gmail.com>
2019-07-29 16:36:11 +0800

    add material

---
+++ nextjs/pages/about.js
@@ -0,0 +1,9 @@
+import Layout from "../layouts/basic";
+
+export default function index() {
+  return (
+    <Layout>
+      <div>about page</div>
+    </Layout>
+  );
+}
---
+++ nextjs/pages/index.js
@@ -0,0 +1,9 @@
+import Layout from "../layouts/basic";
+
+export default function index() {
+  return (
+    <Layout>
+      <div>index page</div>
+    </Layout>
+  );
+}
