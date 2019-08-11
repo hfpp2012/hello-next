6f03bbc6034f448b17e3ddaf4f3ddc90f8a9d2d6
hfpp2012 <hfpp2012@gmail.com>
2019-08-11 13:14:12 +0800

    with apollo

---
+++ hello-apollo/public/precache-manifest.3fea818feec61d9b136922d538f267a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55b42dcefc63e8c90555c0fe864bdc3c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "36d764312dc43b4437f9",
+    "url": "/static/css/main.cfda8877.chunk.css"
+  },
+  {
+    "revision": "89b799375d3d8b772591",
+    "url": "/static/js/2.694d300d.chunk.js"
+  },
+  {
+    "revision": "36d764312dc43b4437f9",
+    "url": "/static/js/main.041743dd.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ab10d000cdf7ee45a57395feee160ad2",
+    "url": "/static/media/logo.ab10d000.png"
+  }
+]);
